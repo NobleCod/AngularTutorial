e42672c7263bae66ad17edc93eb9d37b6c23d9e5
msoliman <msoliman@victorylink.com>
2023-05-24 12:28:42 +0300

    all

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 (function () {
 'use strict';
 
-angular.module('NameCalculator',[])
+angular.module('NameCalculator', [])
 
 .controller('NameCalculatorController', function($scope) {
  
     $scope.name = "";
     $scope.totalValue = 0;
+
+    $scope.displayNums = function () {
+        var totalNameVal = CalculateNumericForString($scope.name);
+        $scope.totalValue = totalNameVal;
+    };
+
+    function CalculateNumericForString(string) {
+        var totalStringValue = 0;
+        for (let i = 0; i < string.length; i++)
+        {
+            totalStringValue += string.charCodeAt(i);
+        }    
     
-}); 
+        return totalStringValue;
+
+    }
+    
+    });
+
 
 })();
