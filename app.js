83b431924406fa1883a1ad29af86ee7aa7e653b7
msoliman <msoliman@victorylink.com>
2023-06-01 12:15:52 +0300

    yes

--- app.js
+++ app.js
@@ -1,39 +1,14 @@
 (function () {
     'use strict';
 
-    angular.module('MsgApp', [])
-        .controller('MsgController', MsgController)
-        .filter('sis', SisFilter);
+    angular.module('CounterApp', [])
+        .controller('CounterController', CounterController);
         
-        MsgController.$inject = ['$scope','$filter','sisFilter'];
-        function MsgController($scope,$filter,sisFilter) {
-            $scope.name = "Mohamed";
-            $scope.AppleCost = .35;
-
-            $scope.saySisMsg = function () {
-                var msg = "it's the weekend bro, let's party!";
-                var output = $filter('uppercase')(msg);
-                output = sisFilter(output)
-                return output
-            }
-            
-            $scope.sayMsg = function () {
-                var msg = "it's the weekend bro, let's party!";
-                var output = $filter('uppercase')(msg);
-                return output
-            }
-
-            $scope.feedme = function () {
-                $scope.fed = "I ate an Apple, I'm Fed!";
-            }
-        };
-
-    function SisFilter() {
-        return function (input) {
-            input = input || "";
-            input = input.replace("BRO", "SIS");
-            return input;
+        CounterController.$inject = ['$scope'];
+        function CounterController($scope) {
+            $scope.onceCounter = 0;
+            $scope.showNumberOfWatchers = function () {
+                console.log("# of Watchers: ", $scope.$$watchersCount);
+            };
         }
-    };
-
 })();
