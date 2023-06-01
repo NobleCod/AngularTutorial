09b0209b6a662588b6154a8adf575ffb885e80b0
msoliman <msoliman@victorylink.com>
2023-06-01 11:18:49 +0300

    commit

--- app.js
+++ app.js
@@ -2,19 +2,38 @@
     'use strict';
 
     angular.module('MsgApp', [])
-        .controller('MsgController', MsgController);
-    
-        MsgController.$inject = ['$scope'];
-        function MsgController($scope) {
+        .controller('MsgController', MsgController)
+        .filter('sis', SisFilter);
+        
+        MsgController.$inject = ['$scope','$filter','sisFilter'];
+        function MsgController($scope,$filter,sisFilter) {
             $scope.name = "Mohamed";
+            $scope.AppleCost = .35;
 
+            $scope.saySisMsg = function () {
+                var msg = "it's the weekend bro, let's party!";
+                var output = $filter('uppercase')(msg);
+                output = sisFilter(output)
+                return output
+            }
+            
             $scope.sayMsg = function () {
-                return "it's the weekend bro, let's party!"
+                var msg = "it's the weekend bro, let's party!";
+                var output = $filter('uppercase')(msg);
+                return output
             }
 
             $scope.feedme = function () {
-                $scope.fed = "I'm Fed!";
+                $scope.fed = "I ate an Apple, I'm Fed!";
             }
         };
+
+    function SisFilter() {
+        return function (input) {
+            input = input || "";
+            input = input.replace("BRO", "SIS");
+            return input;
+        }
+    };
 
 })();
